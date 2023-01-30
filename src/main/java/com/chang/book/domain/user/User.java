4eac28df4fc60c37cc1128c8a918de0db822d31c
HanChangss <hanchangss93@gmail.com>
2023-01-30 17:11:21 +0900

    5장 180쪽 까지 진행된 사항 커밋

---
+++ src/main/java/com/chang/book/domain/user/User.java
@@ -0,0 +1,51 @@
+package com.chang.book.domain.user;
+
+import com.chang.book.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING) // 기본적으로 int로 된 숫자가 저장되어 문자열로 저장될 수 있도록 설정
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+}
--- src/main/java/com/chang/book/service/PostsService.java
+++ src/main/java/com/chang/book/service/PostsService.java
@@ -2,12 +2,16 @@ package com.chang.book.service;
 
 import com.chang.book.domain.posts.Posts;
 import com.chang.book.domain.posts.PostsRepository;
+import com.chang.book.web.dto.PostsListResponseDto;
 import com.chang.book.web.dto.PostsResponseDto;
 import com.chang.book.web.dto.PostsSaveRequestsDto;
 import com.chang.book.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +39,20 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) //데이터를 map으로 담은 뒤 list로 담는 부분
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
